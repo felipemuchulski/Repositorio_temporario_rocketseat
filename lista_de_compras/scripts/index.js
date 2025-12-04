e724c0489714ca172d52329e7d326dfd983e1bba
Felipe Muchulski de Andrades <83315471+felipemuchulski@users.noreply.github.com>
2025-12-04 19:09:55 -0300

    LIST - Começada a criação dos itens da lista

--- lista_de_compras/scripts/index.js
+++ lista_de_compras/scripts/index.js
@@ -1,1 +1,31 @@
+/*
+1 - Criar a lógica do botão de adicionar item
+    Ao clicar no botão ele deve criar um novo item na lista;
+    Deve limpar o conteúdo do input de "Adicionar item"
 
+2 - Criar lógica do botão de remover item
+    Ao clicar no botão ele deve remover o item da lista;
+    E exibir a mensagem de item removido da lista;
+
+
+3 - Pesquisar como posso fazer uma mensagem de log aparecer abaixo da lista
+
+*/
+
+// Pega o botao de adicionar o item
+const btnAddItem = document.getElementById("add_item");
+
+// Pega o id da ul
+const ul_id = document.getElementById("")
+
+
+
+// Cria o input checkbox
+let input_checkbox = document.createElement("input");
+input_checkbox.type = "checkbox";
+input_checkbox.classList.add("input-checkbox");
+
+
+btnAddItem.addEventListener("click", () => {
+    
+})
