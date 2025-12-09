821e2239a727ef03c94c3c1d34f9ef2305d2ada7
Felipe Muchulski de Andrades <lipemuchulski@gmail.com>
2025-12-09 15:45:28 -0300

    REF - Validando somente números no input

---
+++ refund/scripts.js
@@ -0,0 +1,41 @@
+// Selecionando itens do formulario
+const form = document.querySelector("form");
+const amount = document.getElementById("amount");
+const expense = document.getElementById("expense");
+const category = document.getElementById("category");
+
+
+amount.oninput = () => {
+    let value = amount.value.replace(/\D/g, ""); // recebe o valor do input e formata
+
+    // Transformar o valor em centavos
+    value = Number(value) / 100;
+
+    amount.value = formatCurrencyBRL(value); // passa o valor do input formatado
+    
+}
+
+function formatCurrencyBRL(value){
+    // Formata o valor no padrão BRL (Real Brasileiro)
+    value = value.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+
+    })
+
+}
+
+form.onsubmit = (event) => {
+    event.preventDefault()
+
+    const newExpese = {
+        id: new Date().getTime(),
+        expense: expense.value,
+        category_id: category.value,
+        category_name: category.options[category.selectedIndex].text,
+        amount: amount.value,
+        created_at: new Date(),
+    }
+
+    
+}
