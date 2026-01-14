ae0e8bb4302307038ed0fb02f94844f9d6446dbd
Felipe Muchulski de Andrades <83315471+felipemuchulski@users.noreply.github.com>
2026-01-14 19:22:22 -0300

    HD - Testada a busca de agendamentos na API

--- projeto_hair_day/src/modules/schedules/load.js
+++ projeto_hair_day/src/modules/schedules/load.js
@@ -1,11 +1,15 @@
+import { scheduleFetchByDay } from "../../services/schedule-fetch-by-day.js";
 import { hoursLoad } from "../form/hours-load.js"
 
 // Seleciona o input de data
 const selectedDate = document.getElementById("date")
 
-export function scheduleDay(){
+export async function scheduleDay(){
     // Obtém a data do input
     const date = selectedDate.value
+
+    // Buscar na API os agendamentos
+    const dailySchedules = await scheduleFetchByDay({ date })
 
     // Renderiza as horas disponiveis
     hoursLoad({date});
