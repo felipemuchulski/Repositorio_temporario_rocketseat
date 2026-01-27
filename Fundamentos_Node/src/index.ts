a9d32b61469fc5bf3c91586c8d86bdc4de10a18c
Felipe Muchulski de Andrades <lipemuchulski@gmail.com>
2026-01-27 14:16:25 -0300

    TS - Configurado servidor novamente

--- Fundamentos_Node/src/index.ts
+++ Fundamentos_Node/src/index.ts
@@ -1,1 +1,20 @@
-console.log("Rodando em dev com tsx e em prod com node + dist")
+import http from "node:http"
+
+const server = http.createServer((req, res) => {
+    const {method, url} = req
+    res.statusCode = 200
+
+    if (method === "GET" && url === "/products") {
+        return res.writeHead(200).end(JSON.stringify({message: "O método é:", method}))
+    }
+
+    if (method === "POST" && url === "/products") {
+        return res.writeHead(201).end(JSON.stringify({message: "Produto criado"}))
+    }
+
+    
+})
+
+server.listen(3333, () => {
+    console.log("Server is running on port 3333")
+})
