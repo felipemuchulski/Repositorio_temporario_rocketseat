8586173e86d0bf8e3c453b4c33f35256b47620b6
Felipe Muchulski de Andrades <83315471+felipemuchulski@users.noreply.github.com>
2026-01-27 19:21:01 -0300

    TS - Resolvido conflito

--- Fundamentos_Node/src/index.ts
+++ Fundamentos_Node/src/index.ts
@@ -1,1 +1,25 @@
-console.log("Rodando em dev com tsx e em prod com node + dist")
+import http from "node:http"
+import { stringify } from "node:querystring";
+
+const server = http.createServer((req, res) => {
+
+    const { method, url } = req
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+
+
+    if (method === "GET" && url === "/products"){
+        return res.end(JSON.stringify({message: "Lista de produtos"}))
+    }
+
+    if (method === "POST" && url === "/products"){
+        return res.writeHead(201).end(JSON.stringify({message: "Criado produto"}))
+    }
+    return res.end(JSON.stringify({message: "Servidor rodando com TS", method}))
+
+    
+})
+
+server.listen(3000, () => {
+    console.log("Servidor rodando em http://localhost:3000");
+});
