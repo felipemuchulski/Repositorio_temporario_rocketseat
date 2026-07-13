70b53c3c03ea5b73a902b520fcc763b591f1ed1f
Felipe Muchulski de Andrades <83315471+felipemuchulski@users.noreply.github.com>
2026-07-13 11:23:56 -0300

    Feat - Terminado módulo de estilização

--- Fundamentos_React/classroom/src/App.tsx
+++ Fundamentos_React/classroom/src/App.tsx
@@ -1,12 +1,13 @@
-import { Button } from "./components/button"
+import { Button } from "./components/button";
+import styles from "./app.module.css";
 
-export function App(){
+export function App() {
   return (
-    <>
-      <h1>Hello World!</h1>
+    <div className={styles.container}>
       <Button name="Criar" onClick={() => alert("Criar")} />
-      <Button name="Editar" />
+      <Button name="Adicionar" />
+      <span>0</span>
       <Button name="Remover" />
-    </>
-  )
+    </div>
+  );
 }
