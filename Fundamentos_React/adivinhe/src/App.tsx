e82fd40620b48f3761763097963546f59e16fe02
Felipe Muchulski de Andrades <83315471+felipemuchulski@users.noreply.github.com>
2026-07-15 13:57:12 -0300

    fix: Ajustando main do container

--- Fundamentos_React/adivinhe/src/App.tsx
+++ Fundamentos_React/adivinhe/src/App.tsx
@@ -1,20 +1,22 @@
-import styles from "./app.module.css"
-import { Header } from "./components/Header"
-import { Tip } from "./components/Tip"
-import { Letter } from "./components/Letter"
+import styles from "./app.module.css";
+import { Header } from "./components/Header";
+import { Tip } from "./components/Tip";
+import { Letter } from "./components/Letter";
 
 export default function App() {
-
-  function handleRestartGame(){
-    alert("Reiniciar o jogo!")
+  function handleRestartGame() {
+    alert("Reiniciar o jogo!");
   }
   return (
     <div className={styles.container}>
       <main>
         <Header current={5} max={10} onRestart={handleRestartGame}></Header>
         <Tip tip="Uma das linguagens de programação mais utilizadas"></Tip>
-        <Letter value="F"></Letter>
+
+        <div className={styles.word}>
+          <Letter value="F"></Letter>
+        </div>
       </main>
     </div>
-  )
+  );
 }
