b70c52bb1cef6eb527b90511ee27e8ae93aaefbe
Felipe Muchulski de Andrades <83315471+felipemuchulski@users.noreply.github.com>
2026-07-15 14:05:33 -0300

    feat: Criado componente de input

--- Fundamentos_React/adivinhe/src/App.tsx
+++ Fundamentos_React/adivinhe/src/App.tsx
@@ -2,6 +2,7 @@ import styles from "./app.module.css";
 import { Header } from "./components/Header";
 import { Tip } from "./components/Tip";
 import { Letter } from "./components/Letter";
+import { Input } from "./components/Input";
 
 export default function App() {
   function handleRestartGame() {
@@ -15,6 +16,12 @@ export default function App() {
 
         <div className={styles.word}>
           <Letter value="F"></Letter>
+        </div>
+
+        <h4>Palpite</h4>
+
+        <div>
+          <Input autoFocus maxLength={1} placeholder="?" />
         </div>
       </main>
     </div>
