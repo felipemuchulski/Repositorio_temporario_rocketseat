8603353467c3648c0991ca729ba1f374bba5153d
Felipe Muchulski de Andrades <83315471+felipemuchulski@users.noreply.github.com>
2026-07-17 12:05:21 -0300

    feaat: Criado botão com useNavigate

--- Fundamentos_React/react-router/src/pages/Home.tsx
+++ Fundamentos_React/react-router/src/pages/Home.tsx
@@ -1,4 +1,6 @@
+import { useNavigate } from "react-router"
 export function Home(){
+    const navigate = useNavigate()
     return (
         <div>
             <h1>Página Home</h1>
@@ -6,6 +8,8 @@ export function Home(){
             <nav>
                 <a href="/products">Produtos</a>
                 <a href="/products?category=tvs">Categorias</a>
+
+                <button type="button" onClick={() => navigate("/products")}>Ver Produtos</button>
             </nav>
         </div>
     )
