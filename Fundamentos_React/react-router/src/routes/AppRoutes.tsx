abac59ea9e805c30b76d73a981fe3db7e2f329d6
Felipe Muchulski de Andrades <83315471+felipemuchulski@users.noreply.github.com>
2026-07-17 11:33:27 -0300

    feat: Criado home e products

--- Fundamentos_React/react-router/src/routes/AppRoutes.tsx
+++ Fundamentos_React/react-router/src/routes/AppRoutes.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router";
 import { Home } from "../pages/Home";
+import { Products } from "../pages/Products";
 
 export function AppRoutes(){
     return (
         <Routes>
-            <Route path="/" index element={<Home/>}></Route>
+            <Route path="/" index element={<Home/>} />
+            <Route path="/products" element={<Products/>} />
         </Routes>
     )
 }
