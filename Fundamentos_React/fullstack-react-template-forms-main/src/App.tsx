1197f412c6d4781e7074460c729b4f370ac256da
Felipe Muchulski de Andrades <83315471+felipemuchulski@users.noreply.github.com>
2026-07-17 19:02:21 -0300

    feat: Adicionado setName

--- Fundamentos_React/fullstack-react-template-forms-main/src/App.tsx
+++ Fundamentos_React/fullstack-react-template-forms-main/src/App.tsx
@@ -1,15 +1,19 @@
 import "./App.css";
 
+import { useState } from "react";
+
 export default function App() {
+
+  const [name, setName] = useState("")
   return (
     <div>
-      <h1>Evento</h1>
+      <h1>Evento {name}</h1>
 
       <form>
         <input
           type="text"
           placeholder="Nome do evento"
-          onChange={(event) => event.target.value}
+          onChange={(event) => setName(event.target.value)}
         />
         <span className="error">Nome é obrigatório</span>
 
