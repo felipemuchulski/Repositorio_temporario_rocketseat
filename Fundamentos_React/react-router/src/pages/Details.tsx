038700765ba1557073c2cc4e76a00a5f5fdc7ea9
Felipe Muchulski de Andrades <83315471+felipemuchulski@users.noreply.github.com>
2026-07-17 13:33:22 -0300

    feat: Criado cards de produtos

---
+++ Fundamentos_React/react-router/src/pages/Details.tsx
@@ -0,0 +1,14 @@
+import { useParams } from "react-router";
+
+export function Details() {
+  const { id } = useParams()
+  return (
+    <div>
+      <h1>Detalhes</h1>
+
+      <span>
+        ID do produto: <strong>{id}</strong>
+      </span>
+    </div>
+  );
+}
