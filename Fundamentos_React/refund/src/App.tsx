9c15001c6efee75918a20ef785a3ab9549ac8106
Felipe Muchulski de Andrades <83315471+felipemuchulski@users.noreply.github.com>
2026-07-24 11:01:32 -0300

    core: Instalando o react-router e configurando rotas

--- Fundamentos_React/refund/src/App.tsx
+++ Fundamentos_React/refund/src/App.tsx
@@ -1,3 +1,5 @@
+import { Routes } from "./routes"
+
 export function App(){
-  return <h1>Página App</h1>
+  return <Routes />
 }
---
+++ Fundamentos_React/refund/src/pages/SignIn.tsx
@@ -0,0 +1,3 @@
+export function SignIn() {
+    return <h1>SighIn</h1>
+}
