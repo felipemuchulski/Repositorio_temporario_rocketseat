7f8b6ad4b826be2d7e3016b699cf2282b99f1fcd
Felipe Muchulski de Andrades <83315471+felipemuchulski@users.noreply.github.com>
2026-07-24 11:17:46 -0300

    feat: Criando o primeiro campo de authlayout

--- Fundamentos_React/refund/src/routes/auth-routes.tsx
+++ Fundamentos_React/refund/src/routes/auth-routes.tsx
@@ -1,11 +1,14 @@
-import { Routes, Route } from "react-router"
+import { Routes, Route } from "react-router";
 
-import { SignIn } from "../pages/SignIn"
+import { AuthLayout } from "../components/AuthLayout";
+import { SignIn } from "../pages/SignIn";
 
-export function AuthRoutes(){
-    return (
-        <Routes>
-            <Route path="/" element={<SignIn />} />
-        </Routes>
-    )
+export function AuthRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<AuthLayout />}>
+        <Route path="/" element={<SignIn />} />
+      </Route>
+    </Routes>
+  );
 }
