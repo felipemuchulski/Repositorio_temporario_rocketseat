4d148282a4aaf53c83d1b68804978ceff059dc1e
Felipe Muchulski de Andrades <lipemuchulski@gmail.com>
2026-07-28 12:45:27 -0300

    feat: Criado o componente de input

---
+++ Fundamentos_React/refund/src/components/Input.tsx
@@ -0,0 +1,22 @@
+type Props = React.ComponentProps<"input"> & {
+  legend?: string;
+};
+
+export function Input({ legend, ...rest }: Props) {
+  return (
+    <fieldset className="flex flex-1 max-h-20 focus-within:text-green-100">
+      {legend && (
+        <legend className="uppercase text-xs text-gray-200 mb-2 text-inherit">
+          {legend}
+        </legend>
+      )}
+
+      <input
+        type="text"
+        {...rest}
+        className="w-full h-12 rounded-lg border border-gray-300 px-4 text-sm
+      text-gray-100 bg-transparent outline-none focus:border-2 focus:border-green-100 placeholder-gray-300"
+      />
+    </fieldset>
+  );
+}
--- Fundamentos_React/refund/src/pages/SignIn.tsx
+++ Fundamentos_React/refund/src/pages/SignIn.tsx
@@ -1,3 +1,23 @@
+import { Input } from "../components/Input";
+
+export { Input } from "../components/Input";
+
 export function SignIn() {
-    return <h1>SighIn</h1>
+  return (
+    <form action="" className="w-full flex flex-col gap-4">
+      <Input
+        required
+        legend="E-mail"
+        type="email"
+        placeholder="seu@email.com"
+      />
+
+      <Input
+        required
+        legend="Senha"
+        type="password"
+        placeholder="123456"
+      />
+    </form>
+  );
 }
