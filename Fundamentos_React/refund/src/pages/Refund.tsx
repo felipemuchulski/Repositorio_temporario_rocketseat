110120eb41cf00c88e4169daa19f22e6863a00a5
Felipe Muchulski de Andrades <lipemuchulski@gmail.com>
2026-07-28 15:36:15 -0300

    feat: Criado componente de upload

--- Fundamentos_React/refund/src/pages/Refund.tsx
+++ Fundamentos_React/refund/src/pages/Refund.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { CATEGORIES, CATEGORIES_KEYS } from "../utils/categories";
 import { Input } from "../components/Input";
 import { Select } from "../components/Select";
+import { Upload } from "../components/Upload";
 
 export function Refund() {
   const [category, setCategory] = useState("");
@@ -37,6 +38,8 @@ export function Refund() {
 
         <Input legend="Valor" required />
       </div>
+
+      <Upload />
     </form>
   );
 }
