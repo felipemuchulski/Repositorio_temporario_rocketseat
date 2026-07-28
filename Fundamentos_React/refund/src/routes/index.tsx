7e8fd75c81b37daf38fd348a7f9ebe36b78fe59e
Felipe Muchulski de Andrades <lipemuchulski@gmail.com>
2026-07-28 14:14:57 -0300

    feat: Criada base inicial de rotas refund

--- Fundamentos_React/refund/src/routes/index.tsx
+++ Fundamentos_React/refund/src/routes/index.tsx
@@ -1,11 +1,11 @@
 import { BrowserRouter } from "react-router";
 
 import { AuthRoutes } from "./AuthRoutes";
-
+import { EmployeeRoutes } from "./EmployeeRoutes";
 export function AppRoutes() {
     return (
         <BrowserRouter>
-            <AuthRoutes />
+            <EmployeeRoutes />
         </BrowserRouter>
     )
 }
