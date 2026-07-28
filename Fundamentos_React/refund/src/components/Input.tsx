9eb2ca28110948457f51fd676d1df2d988f78b7f
Felipe Muchulski de Andrades <lipemuchulski@gmail.com>
2026-07-28 12:47:57 -0300

    fix: Ajuste no input

--- Fundamentos_React/refund/src/components/Input.tsx
+++ Fundamentos_React/refund/src/components/Input.tsx
@@ -2,7 +2,7 @@ type Props = React.ComponentProps<"input"> & {
   legend?: string;
 };
 
-export function Input({ legend, ...rest }: Props) {
+export function Input({ legend, type = "text", ...rest }: Props) {
   return (
     <fieldset className="flex flex-1 max-h-20 focus-within:text-green-100">
       {legend && (
@@ -12,10 +12,10 @@ export function Input({ legend, ...rest }: Props) {
       )}
 
       <input
-        type="text"
-        {...rest}
+        type={type}
         className="w-full h-12 rounded-lg border border-gray-300 px-4 text-sm
       text-gray-100 bg-transparent outline-none focus:border-2 focus:border-green-100 placeholder-gray-300"
+        {...rest}
       />
     </fieldset>
   );
