72b3405824f65313941fc08cb4aaf9c5fd768ead
Felipe Muchulski de Andrades <lipemuchulski@gmail.com>
2026-07-28 15:58:17 -0300

    fix: Terminando o Confirm

--- Fundamentos_React/refund/src/pages/Confirm.tsx
+++ Fundamentos_React/refund/src/pages/Confirm.tsx
@@ -1,8 +1,23 @@
-import { Navigate, useLocation } from "react-router"
-export function Confirm(){
-    const location = useLocation()
-    
-    if(!location.state?.fromSubmit) {
-        return <Navigate to='/' />
-    }
+import { Navigate, useLocation } from "react-router";
+import okSvg from "../assets/ok.svg";
+
+export function Confirm() {
+  const location = useLocation();
+
+  if (!location.state?.fromSubmit) {
+    return <Navigate to="/" />;
+  }
+
+  return (
+    <div>
+      <h1>Solicitação enviada</h1>
+
+      <img src={okSvg} alt="Ok" className="w-28" />
+
+      <p>
+        Agora é apenas aguardar! Sua solicitação será analisada e, em brevre, o
+        setor financeiro entrará em contato com você.
+      </p>
+    </div>
+  );
 }
