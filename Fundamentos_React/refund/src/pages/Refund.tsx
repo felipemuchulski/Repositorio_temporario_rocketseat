9d31516751e3d5bec0fc3455c89507d67e2e6efe
Felipe Muchulski de Andrades <lipemuchulski@gmail.com>
2026-07-28 15:13:32 -0300

    fix: Criada separação dos inputs

--- Fundamentos_React/refund/src/pages/Refund.tsx
+++ Fundamentos_React/refund/src/pages/Refund.tsx
@@ -1,27 +1,42 @@
 import { useState } from "react";
-import { CATEGORIES, CATEGORIES_KEYS } from "../utils/categories"
+import { CATEGORIES, CATEGORIES_KEYS } from "../utils/categories";
 import { Input } from "../components/Input";
-import { Select } from "../components/Select"
+import { Select } from "../components/Select";
 
-export function Refund(){
-    const [category, setCategory] = useState("")
-    return <form className="bg-gray-500 w-full rounded-xl flex flex-col p-10 gap-6
-    lg:min-w[512px]">
-        <header>
-            <h1 className="text-xl font-bold text-gray-100">Solicitação de reembolso</h1>
-            <p className="text-sm text-gray-200 mt-2 mb-4">Dados da despesa para solicitar reembolso.</p>
-        </header>
+export function Refund() {
+  const [category, setCategory] = useState("");
+  return (
+    <form
+      className="bg-gray-500 w-full rounded-xl flex flex-col p-10 gap-6
+    lg:min-w[512px]"
+    >
+      <header>
+        <h1 className="text-xl font-bold text-gray-100">
+          Solicitação de reembolso
+        </h1>
+        <p className="text-sm text-gray-200 mt-2 mb-4">
+          Dados da despesa para solicitar reembolso.
+        </p>
+      </header>
 
-        <Input
-        required
-        legend="Nome da solicitação" />
+      <Input required legend="Nome da solicitação" />
 
-        <Select required legend="Categoria" value={category} onChange={(event) => setCategory(event.target.value)}>
-            {
-                CATEGORIES_KEYS.map((category) => (
-                    <option key={category} value={category}>{CATEGORIES[category].name}</option>
-                ))
-            }
+      <div className="flex gap-4">
+        <Select
+          required
+          legend="Categoria"
+          value={category}
+          onChange={(event) => setCategory(event.target.value)}
+        >
+          {CATEGORIES_KEYS.map((category) => (
+            <option key={category} value={category}>
+              {CATEGORIES[category].name}
+            </option>
+          ))}
         </Select>
+
+        <Input legend="Valor" required />
+      </div>
     </form>
+  );
 }
