8b0babd83f13b91c5946e4016207b182c89f309a
Felipe Muchulski de Andrades <lipemuchulski@gmail.com>
2026-07-28 15:11:04 -0300

    feat: Termiada estilização do select

--- Fundamentos_React/refund/src/pages/Refund.tsx
+++ Fundamentos_React/refund/src/pages/Refund.tsx
@@ -1,7 +1,10 @@
+import { useState } from "react";
+import { CATEGORIES, CATEGORIES_KEYS } from "../utils/categories"
 import { Input } from "../components/Input";
 import { Select } from "../components/Select"
 
 export function Refund(){
+    const [category, setCategory] = useState("")
     return <form className="bg-gray-500 w-full rounded-xl flex flex-col p-10 gap-6
     lg:min-w[512px]">
         <header>
@@ -13,6 +16,12 @@ export function Refund(){
         required
         legend="Nome da solicitação" />
 
-        <Select required legend="Categoria" />
+        <Select required legend="Categoria" value={category} onChange={(event) => setCategory(event.target.value)}>
+            {
+                CATEGORIES_KEYS.map((category) => (
+                    <option key={category} value={category}>{CATEGORIES[category].name}</option>
+                ))
+            }
+        </Select>
     </form>
 }
