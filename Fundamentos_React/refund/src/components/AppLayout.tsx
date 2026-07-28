6b468cd98b0728dd39a47ab8e857691302ec3373
Felipe Muchulski de Andrades <lipemuchulski@gmail.com>
2026-07-28 14:27:25 -0300

    feat: Criada rota de Employe e o primeiro layout

---
+++ Fundamentos_React/refund/src/components/AppLayout.tsx
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router";
+
+export function AppLayout() {
+    return (
+        <div className="w-screen h-screen bg-gray-400 flex flex-col items-center 
+        text-gray-100">
+            <main className="p-3 w-full mb:w-auto">
+                <Outlet />
+            </main>
+        </div>
+    )
+}
