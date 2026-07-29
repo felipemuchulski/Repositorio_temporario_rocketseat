1dbfd24500066ca35e2e206e415a4b2146211abf
Felipe Muchulski de Andrades <lipemuchulski@gmail.com>
2026-07-29 11:47:52 -0300

    feat: Configurado o twMerge e o clsx

--- Fundamentos_React/refund/src/components/Button.tsx
+++ Fundamentos_React/refund/src/components/Button.tsx
@@ -1,10 +1,20 @@
-
+import { ClassMerge } from "../utils/classMerge"
 type Props = React.ComponentProps<"button"> & {
     isLoading?: boolean
+    variant?: "base" | "icon"
 }
 
+const variants = {
+    button: {
+        base: "h-12",
+        icon: "h-12 w-12",
+    },
+}
 
-export function Button({ children, isLoading, type="button", ...rest}: Props){
-    return <button type={type} disabled={isLoading} className="flex items-center justify-center bg-green-100 rounded-lg text-white
-    cursor-pointer hover:bg-green-200 transition ease-linear disabled:opacity-50 disabled:cursor-progress h-12" {...rest}>{children}</button>
+export function Button({ children, isLoading, type="button", variant="base", ...rest}: Props){
+    return <button
+     type={type}
+     disabled={isLoading}
+     className={ClassMerge("flex items-center justify-center bg-green-100 rounded-lg text-white cursor-pointer hover:bg-green-200 transition ease-linear disabled:opacity-50 disabled:cursor-progress", variants.button[variant])}
+     {...rest}>{children}</button>
 }
--- Fundamentos_React/refund/src/pages/Dashboard.tsx
+++ Fundamentos_React/refund/src/pages/Dashboard.tsx
@@ -1,21 +1,33 @@
-import { useState } from "react"
-import { Input } from "../components/Input"
+import { useState } from "react";
+import { Button } from "../components/Button";
+import { Input } from "../components/Input";
+import searchSvg from "../assets/search.svg";
 
 export function Dashboard() {
-    const [name, setName] = useState("")
+  const [name, setName] = useState("");
 
-    function fecthRefunds(event: React.FormEvent){
-        event.preventDefault()
-    }
+  function fecthRefunds(event: React.FormEvent) {
+    event.preventDefault();
+  }
 
-    return(
-        <div className="bg-gray-500 rounded-xl p-10 md:min-w-[768px]">
-            <h1 className="text-gray-100 font-bold text-xl flex-1">Solicitações</h1>
+  return (
+    <div className="bg-gray-500 rounded-xl p-10 md:min-w-[768px]">
+      <h1 className="text-gray-100 font-bold text-xl flex-1">Solicitações</h1>
 
-            <form onSubmit={fecthRefunds} className="flex flex-1 items-center justify-between pb-6 border-b-[1px] 
-            border-gray-400 md:flex-row gap-2 mt-6">
-                <Input placeholder="Pesquisar pelo nome" onChange={(event) => setName(event.target.value)}/>
-            </form>
-        </div>
-    )
+      <form
+        onSubmit={fecthRefunds}
+        className="flex flex-1 items-center justify-between pb-6 border-b-[1px] 
+            border-gray-400 md:flex-row gap-2 mt-6"
+      >
+        <Input
+          placeholder="Pesquisar pelo nome"
+          onChange={(event) => setName(event.target.value)}
+        />
+
+        <Button variant="icon">
+            <img src={searchSvg} alt="Ícone de pesquisa" />
+        </Button>
+      </form>
+    </div>
+  );
 }
