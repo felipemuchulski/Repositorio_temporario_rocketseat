8d1dae33e7bdfb5af64cc9668847101ec586ffaf
Felipe Muchulski de Andrades <lipemuchulski@gmail.com>
2026-07-29 10:59:43 -0300

    feat: Criado o primeiro modelo de rotas

--- Fundamentos_React/refund/src/routes/index.tsx
+++ Fundamentos_React/refund/src/routes/index.tsx
@@ -2,10 +2,11 @@ import { BrowserRouter } from "react-router";
 
 import { AuthRoutes } from "./AuthRoutes";
 import { EmployeeRoutes } from "./EmployeeRoutes";
+import { ManagerRoutes } from "./ManagerRoutes";
 export function AppRoutes() {
     return (
         <BrowserRouter>
-            <EmployeeRoutes />
+            <ManagerRoutes />
         </BrowserRouter>
     )
 }
